77d8e52b6a596129b779332e700e413537e93387
Ravi Sheoran <ravisheoran@Ravis-MacBook-Pro.local>
2024-06-16 17:46:57 +0530

    change

--- src/component/ToolSelectionArea.js
+++ src/component/ToolSelectionArea.js
@@ -1,26 +1,3 @@
-// import React from 'react'
-
-// export default function Footer({mode,setMode}) {
-  
-//   const x1 = 10;
-//   const y1 = 10;
-//   const x2 = 50;
-//   const y2 = 10;
-//   const toggleMode = () => {
-//     setMode(mode === 'draw' ? 'pan' : 'draw');
-//   };
-//   return (
-//     <div className="footer">
-//     <svg width="100" height="20" cursor={"pointer"} onClick={toggleMode}>
-//       <line x1={x1} y1={y1} x2={x2} y2={y2} stroke={"black"} strokeWidth={mode==="draw"?"3":"1"} />
-//       <circle cx={x1} cy={y1} r="3" fill="black" />
-//       <circle cx={x2} cy={y2} r="3" fill="black" />
-//     </svg>
-//     </div>
-//   )
-// }
-
-
     
 import React, { useEffect, useRef } from 'react';
 
